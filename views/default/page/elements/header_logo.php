0aac0e6d97b48c0aa28d03f49e9aaf55b038e742
Renato <renato.navarrete2020@alu.uct.cl>
2021-10-05 20:40:35 -0300

    Cambio logo
    agregar texto

--- views/default/page/elements/header_logo.php
+++ views/default/page/elements/header_logo.php
@@ -1,3 +1,6 @@
 <a href="http://localhost/sites/elgg/IntegracionElgg">
 <img src="http://localhost/sites/elgg/mod/IntegracionElgg/images/lol.png">
+<section>
+		<h1>Callate el hocico</h1>
+	</section>
 </a>
