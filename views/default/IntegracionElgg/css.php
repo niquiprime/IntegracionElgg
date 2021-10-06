85b40b37e72fb6ce988168ca3fbba1d03f70dd85
nicolas <nchandia2020@alu.uct.cl>
2021-10-06 18:19:47 -0300

    Ajuste de colores y letras

--- views/default/IntegracionElgg/css.php
+++ views/default/IntegracionElgg/css.php
@@ -5,6 +5,17 @@
 	background-color: #C82A54;
 }
 .elgg-sidebar{
-	background-color: #fff;
+	background-color: #f2f2f2;
 	font: 120% Arial, Helvetica, sans-serif;
+}
+.elgg-page-body{
+	margin: 0;
+	padding: 0;
+	font-family: Helvetica, Arial, sans-serif;
+	background: #f2f2f2; 
+	font-size: 1em;
+	line-height: 1.5em;
+}
+.elgg-button-action{
+	background: #C82A54; 
 }
